b111214bec12100fefd3d1390ee3eded285f270e
Rakshith Kalmadi <58518378+rakshithkalmadi@users.noreply.github.com>
2026-01-21 00:03:21 +0530

    Init

---
+++ main.py
@@ -0,0 +1,8 @@
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+load_dotenv()
+
+model = init_chat_model("google_genai:gemini-2.5-flash-lite")
+
+response = model.invoke("Why do parrots talk?")
+print(response)
